db7e0d84914df2a57c42769cbb18ef77231179a3
Garvas <rogachev.seal@gmail.com>
2015-09-02 23:14:50 +0300

    New file. Sort target dir

---
+++ GetFile.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 15:37:49 2015
+
+@author: RSA
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, re
+
+targetFolder = u'Z:/SiteliteIMG/Ленобл_Ресурс-П'
+
+ext = re.compile(r"^.+\.(?:hdf|temp|dbf|shp|shx|smd|tif|tiff|txt|log)$", re.IGNORECASE + re.UNICODE)
+
+def sort_dir(targetFolder, ext):
+
+    print 'Processing ' + targetFolder
+
+    for fileName in os.listdir(targetFolder):
+        filePath = os.path.join(targetFolder, fileName)
+        if os.path.isfile(filePath):
+            if ext.match(fileName):
+                dirPath = os.path.join(targetFolder, (os.path.splitext(fileName)[0]).strip())
+                if not os.path.exists(dirPath):
+                    os.mkdir(dirPath)
+                    os.rename(filePath, os.path.join(dirPath, fileName))
+                else:
+                    os.rename(filePath, os.path.join(dirPath, fileName))
+
+    print 'Done!'
+
+#sort_dir(targetFolder,ext)
